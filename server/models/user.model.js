6fe42e0b57caeea62e25b7b21b5e337b4ef0548d
jony <jonybhattcharjee886@gmail.com>
2024-10-07 01:54:37 +0600

    Modified user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,8 +3,8 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema(
   {
-    fisrtname: { type: String, required: true },
-    lasttname: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["user", "admin"], default: "user" },
