04a97dea2065f726c828cb3f9df915d412978b50
jony <jonybhattcharjee886@gmail.com>
2024-10-07 01:59:40 +0600

    Added middleware

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+exports.authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    req.userRole = decoded.role;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+};
