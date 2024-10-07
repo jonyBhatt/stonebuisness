5004856bd95d3bfa0118cb99c64431fa907a6d29
jony <jonybhattcharjee886@gmail.com>
2024-10-08 01:18:10 +0600

    Added login and register route

--- stonebuisness/src/lib/router.tsx
+++ stonebuisness/src/lib/router.tsx
@@ -1,6 +1,8 @@
 import App from "@/App";
 import { Layout } from "@/components/Layout";
 import About from "@/pages/About";
+import Login from "@/pages/auth/login";
+import Register from "@/pages/auth/register";
 import BusinessAndProducts from "@/pages/BusinessAndProducts";
 import ContactPage from "@/pages/Contact";
 import Management from "@/pages/Management";
@@ -30,6 +32,14 @@ const router = createBrowserRouter([
       {
         path: "/contact",
         element: <ContactPage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
       },
     ],
   },
