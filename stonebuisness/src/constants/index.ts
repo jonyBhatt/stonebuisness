ffd97f66324772d9bccfdf5c2d342041a93b9853
jony <jonybhattcharjee886@gmail.com>
2024-10-20 23:24:24 +0600

    Constant

---
+++ stonebuisness/src/constants/index.ts
@@ -0,0 +1,56 @@
+import Boulderstone from "../assets/boulder.jpg";
+import Curshedstone from "../assets/crushed.png";
+import LimeStone from "../assets/limestone.png";
+import Sand from "../assets/sand.jpg";
+import SingleStone from "../assets/signlestone.png";
+import StoneDust from "../assets/stonedust.jpg";
+export const products = [
+  {
+    name: "Crushed Stone",
+    origin: "Chhatak, Bangladesh",
+    color: "Grey",
+    sizes: "10 mm, 20 mm, 40 mm",
+    capacity: "40,000 tons - 70,000 tons",
+    image: Curshedstone,
+  },
+  {
+    name: "Boulder Stones",
+    origin: "Jaflong, Bangladesh",
+    color: "Natural Stone Color",
+    sizes: "100 mm - 500 mm (varied sizes available)",
+    capacity: "20,000 tons - 50,000 tons",
+    image: Boulderstone,
+  },
+  {
+    name: "Chhatak Sand",
+    origin: "Chhatak, Bangladesh",
+    color: "Natural Sand Color",
+    sizes: "Fine, Medium, Coarse",
+    capacity: "50,000 tons - 80,000 tons",
+    image: Sand,
+  },
+  {
+    name: "Stone Dust",
+    origin: "Chhatak, Bangladesh",
+    color: "Grey",
+    sizes: "Fine Powder",
+    capacity: "30,000 tons - 60,000 tons",
+    image: StoneDust,
+  },
+  {
+    name: "Single Stone",
+    origin: "Jaflong, Bangladesh",
+    color: "Natural Stone Color",
+    sizes: "300 mm - 600 mm (varied sizes available)",
+    capacity: "10,000 tons - 30,000 tons",
+    image: SingleStone,
+  },
+  {
+    name: "Lime Stone",
+    origin: "Chhatak, Bangladesh",
+    color: "Off-white, beigh to light grey",
+    sizes: "10 mm, 20 mm, 40 mm",
+    capacity: "40,000 tons - 70,000 tons",
+    image: LimeStone,
+  },
+];
